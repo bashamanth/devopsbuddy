7118e24b9013e932921c797382f40d8ad0234675
bashamanth <bashamanth@gmail.com>
2018-08-26 17:11:47 +0530

    Added index controller

--- src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
+++ src/main/java/com/devopsbuddy/controllers/HelloWorldController.java
@@ -6,10 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloWorldController {
 	
-	@RequestMapping("/")
-	public String sayHello() {
-		return "index";
-	}
+
 	
 	
 }
